e110a0bd90a507eeaca30c26803782eb39865056
Anmol Jamba <anmoljamba18@gmail.com>
2025-11-02 23:39:31 +0530

    feat: add PostgreSQL insert logic in Kafka consumer

--- src/kafka/consumer.js
+++ src/kafka/consumer.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Kafka } = require("kafkajs");
-const handleError = require("../helpers/errorHandler"); 
+const { upsertCandle } = require("../services/candleService");
 
 async function runConsumer() {
   const kafka = new Kafka({
@@ -8,30 +8,26 @@ async function runConsumer() {
     brokers: [process.env.BROKER_NAME],
   });
 
-  const consumer = kafka.consumer({
-    groupId: "stockpulse-stream-group", 
+  const consumer = kafka.consumer({ groupId: "stockpulse-stream-group" });
+
+  await consumer.connect();
+  console.log("Kafka Consumer connected...");
+
+  await consumer.subscribe({ topic: process.env.TOPIC, fromBeginning: false });
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const tick = JSON.parse(message.value.toString());
+
+      console.log(`Tick: ${tick.symbol} | Price: ${tick.price}`);
+
+      try {
+        await upsertCandle(tick);
+      } catch (err) {
+        handleError(err, "KAFKA_CONSUMER_PARSE");
+      }
+    },
   });
-
-  try {
-    await consumer.connect();
-    console.log("Kafka Consumer connected...");
-
-    await consumer.subscribe({ topic: process.env.TOPIC, fromBeginning: false });
-
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        try {
-          const data = JSON.parse(message.value.toString());
-          console.log("Tick Received:", data);
-        } catch (err) {
-          handleError(err, "KAFKA_CONSUMER_PARSE");
-        }
-      },
-    });
-
-  } catch (error) {
-    handleError(error, "KAFKA_CONSUMER");
-  }
 }
 
 runConsumer();
