9aecbbcd75fccffa94b1192304384022431710ef
Anmol Jamba <anmoljamba18@gmail.com>
2025-11-04 17:55:53 +0530

    feat: add redis configuration for client connection

---
+++ src/config/redis.js
@@ -0,0 +1,12 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+redisClient.on("error", (err) => console.error("Redis Error", err));
+
+module.exports = { redisClient };
